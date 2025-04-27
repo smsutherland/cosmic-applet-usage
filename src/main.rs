105c27737a323d203a3f16482635ccf6d1c6e583
Sagan Sutherland <sagan@farthinghalearms.com>
2025-04-26 22:51:00 -0400

    make into applet

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,6 @@ fn main() -> cosmic::iced::Result {
     // Enable localizations to be applied.
     i18n::init(&requested_languages);
 
-    // Settings for configuring the application window and iced runtime.
-    let settings = cosmic::app::Settings::default().size_limits(
-        cosmic::iced::Limits::NONE
-            .min_width(360.0)
-            .min_height(180.0),
-    );
-
     // Starts the application's event loop with `()` as the application's flags.
-    cosmic::app::run::<app::AppModel>(settings, ())
+    cosmic::applet::run::<app::UsageApp>(())
 }
